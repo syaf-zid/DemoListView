8e2c67de5fde0e0c5223efd359d176e7796607c2
syafzid <18052270@myrp.edu.sg>
2020-05-04 10:54:35 +0800

    demo list view toast completed

--- app/src/main/java/com/example/demolistview/MainActivity.java
+++ app/src/main/java/com/example/demolistview/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.demolistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -30,5 +33,16 @@ public class MainActivity extends AppCompatActivity {
         // each row and the food String array together
         aa = new FoodAdapter(this, R.layout.row, food);
         lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Food selectedFood = food.get(position);
+
+                Toast.makeText(MainActivity.this,
+                        selectedFood.getName() + " Star: " + selectedFood.isStar(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
